e2bcd031631916b2a695b3cc71be7f40452c81c8
Tony Smith <46821595+adavidsmith5@users.noreply.github.com>
2021-02-08 11:12:34 -0500

    Reading data, changing column names
    Reading the data in from the tables given, changing column names to match the variables and adding column titles for subjectid and activity to make it readable.

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,32 @@
+# Installing/loading packages necessary for cleaning the data
+install.packages("plyr")
+install.packages("dplyr")
+library(plyr)
+library(dplyr)
+
+# Gathering data from text files in the working directory
+# Testing and training data for each sample
+xtest <- read.table("X_test.txt")
+xtrain <- read.table("X_train.txt")
+
+# Testing and training labels for each sample (Activities 1-6 as described in the code book)
+ytest <- read.table("y_test.txt")
+ytrain <- read.table("y_train.txt")
+
+# Subject id's for the 30 different subjects in the study
+trainsubject <- read.table("subject_train.txt")
+testsubject <- read.table("subject_test.txt")
+
+# Variable names for the testing/training data
+features <- read.table("features.txt")
+
+# Changing column names to the variable names in the features file
+names(xtest) <- features[["V2"]]
+names(xtrain) <- features[["V2"]]
+
+# Changing column names for the subject id and activity columns
+names(trainsubject) <- "subjectid"
+names(testsubject) <- "subjectid"
+names(ytrain) <- "activity"
+names(ytest) <- "activity"
 
